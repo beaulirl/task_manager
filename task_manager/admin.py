a184d8190a4bc49d437c1a920c467e2194fb4ec7
Elena Reutova <Elena.Reutova@artezio.com>
2018-07-23 19:33:32 +0300

    Add small syntax changes

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+
 from .models import Project, Description, Comment, Task, Status
 
 admin.site.register(Project)
--- task_manager/models.py
+++ task_manager/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-
 class Project(models.Model):
     name = models.CharField(max_length=200, default='inner')
 
